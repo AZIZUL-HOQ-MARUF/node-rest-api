cc2ba001cc058852f0dd25caeec7ee79693ea012
Azizul <azizulmaruf@gmail.com>
2020-04-17 20:54:41 +0600

    allowed cross origin, headers, methods

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ app.use(morgan('dev')); //log routing for dev
 app.use(bodyParser.urlencoded({ extended: false }));// accepting application/x-www-form-urlencoded
 app.use(bodyParser.json()); // accepting application/json
 
+//Allow CORS, Headers, Methods
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use(require('./app/router'));//using router module
 
 const errorHandler = require('./app/error-handler'); //Error Handler
