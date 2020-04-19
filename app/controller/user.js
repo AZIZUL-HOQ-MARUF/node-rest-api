4c257fcb7cf43bd44ad86a3c2ed79dafe0b2d301
Azizul <azizulmaruf@gmail.com>
2020-04-20 01:16:12 +0600

    added login route and generated jwt token

--- app/controller/user.js
+++ app/controller/user.js
@@ -2,10 +2,11 @@ const express = global.express, router = express.Router();
 const User = require('../models/user'),
     mongoose = require('mongoose'),
     bcrypt = require('bcrypt'),
-    AppResponse = require('../app-response');
+    AppResponse = require('../app-response'),
+    jwt = require('jsonwebtoken');
 
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', (req, res) => {
     User.find({ email: req.body.email })
         .exec()
         .then(user => {
@@ -36,7 +37,38 @@ router.post('/signup', (req, res, next) => {
             res.status(500).json(new AppResponse(res.statusCode, null, err.message));
         });
 });
-router.delete('/:userId', (req, res, next) => {
+router.post('/login', (req, res) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (!user.length) {
+                return res.status(401).json(new AppResponse(res.statusCode, null, 'Email or Password Invalid'));
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json(new AppResponse(res.statusCode, null, 'Email or Password Invalid'));
+                }
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            id: user[0]._id
+                        },
+                        process.env.JWT_PRIVATE_KEY,
+                        {
+                            expiresIn: "1h"
+                        }
+                    )
+                    return res.status(200).json(new AppResponse(res.statusCode, { token: token }, 'Auth Success'));
+                }
+                res.status(401).json(new AppResponse(res.statusCode, null, 'Email or Password Invalid'));
+            })
+        })
+        .catch(err => {
+            res.status(500).json(new AppResponse(res.statusCode, null, err.message));
+        });
+})
+router.delete('/:userId', (req, res) => {
     User.remove({ _id: req.params.userId })
         .exec()
         .then(result => {
